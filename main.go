9d4b4149ba99b8bd056986bdf46352ee439ee018
gregory <10611760+gregoryguillou@users.noreply.github.com>
2021-05-26 21:21:56 +0200

    Add logs to the HTTP server

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
 type webServer struct{}
@@ -11,11 +13,18 @@ func (h *webServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "red"}`))
 }
 
+func log(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+		fmt.Printf("%s %s %s\n", time.Now().Format("15:04:05"), r.Method, r.URL.Path)
+	})
+}
+
 func main() {
 	addr := "localhost:8080"
 	if os.Getenv("ADDR") != "" {
 		addr = os.Getenv("ADDR")
 	}
 	h := &webServer{}
-	http.ListenAndServe(addr, h)
+	http.ListenAndServe(addr, log(h))
 }
